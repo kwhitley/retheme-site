d1e9dc5c78bcab30e19ca7ea5db5110cc51d441f
Kevin Whitley <krwhitley@gmail.com>
2022-02-23 14:34:09 -0600

    ripped out supergeneric

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,19 +1,3 @@
-<script>
-  import { randomItem } from 'supergeneric/randomItem'
-
-  const message = randomItem([
-    `Yep, that's my name.`,
-    `Imagine me bragging about myself here...`,
-    `But who am I really?`,
-    `[insert awards and achievements here]`,
-    `Let's play a game of Deep Rock!`,
-  ])
-</script>
-
 <p>
-  {message}
-</p>
-
-<p>
-
+  Bio here...
 </p>
